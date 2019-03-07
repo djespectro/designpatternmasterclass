ad69a7034fef06023fe5ab489e7a2c4de4ce2a03
mac <djespectro@hotmail.com>
2019-03-06 20:32:54 -0800

    added description to decorator, singleton and strategy package-info

--- src/main/java/com/mac/designpatternsmasterclass/singleton/package-info.java
+++ src/main/java/com/mac/designpatternsmasterclass/singleton/package-info.java
@@ -1,4 +1,18 @@
-package com.mac.designpatternsmasterclass.singleton;
+/**
+ * Singleton pattern
+ *<p>
+ * In software engineering, the singleton pattern is a software design pattern that restricts the instantiation
+ * of a class to one. This is useful when exactly one object is needed to coordinate actions across the system.
+ * The term comes from the mathematical concept of a singleton.
+ * </p>
+ *
+ * <p>
+ * Critics consider the singleton to be an anti-pattern in that it is frequently used in scenarios where it is not
+ * beneficial, introduces unnecessary restrictions in situations where a sole instance of a class is not actually
+ * required, and introduces global state into an application
+ *</p>
+ *
+ * @author mac
+ */
 
-//Ensures a class jas only on instance and
-//provide a global point of access to it.
+package com.mac.designpatternsmasterclass.singleton;
---
+++ src/main/java/com/mac/designpatternsmasterclass/strategy/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * Strategy pattern
+ * <p>
+ * In computer programming, the strategy pattern (also known as the policy pattern) is a behavioral software design
+ * pattern that enables selecting an algorithm at runtime. Instead of implementing a single algorithm directly, code
+ * receives run-time instructions as to which in a family of algorithms to use.
+ * </p>
+ *
+ * <p>
+ * Strategy lets the algorithm vary independently from clients that use it. Strategy is one of the patterns included
+ * in the influential book Design Patterns by Gamma et al.[3] that popularized the concept of using design patterns to
+ * describe how to design flexible and reusable object-oriented software. Deferring the decision about which algorithm
+ * to use until runtime allows the calling code to be more flexible and reusable.
+ * </p>
+ * @author mac
+ *
+ */
+
+package com.mac.designpatternsmasterclass.strategy;
