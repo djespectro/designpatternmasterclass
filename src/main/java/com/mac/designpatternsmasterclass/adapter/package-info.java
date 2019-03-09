95ca744bfa25bb9976761b6c1e78d50fedf07832
mac <djespectro@hotmail.com>
2019-03-08 19:09:36 -0800

    Adapter pattern: added description package-info

--- src/main/java/com/mac/designpatternsmasterclass/adapter/package-info.java
+++ src/main/java/com/mac/designpatternsmasterclass/adapter/package-info.java
@@ -2,9 +2,12 @@
  * Adapter Pattern
  *
  * <p>
- *
+ *  In software engineering, the adapter pattern is a software design pattern (also known as wrapper, an alternative
+ *  naming shared with the decorator pattern) that allows the interface of an existing class to be used as another
+ *  interface. It is often used to make existing classes work with others without modifying their source code.
  * </p>
  *
  * @author mac
  */
+
 package com.mac.designpatternsmasterclass.adapter;
